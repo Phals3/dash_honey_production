747d68d6d6be4efb39bf227f9a7d05df93095b05
Phals3 <phalse3@gmail.com>
2021-06-07 23:56:12 +0200

    changed map layout

--- app/apps/map.py
+++ app/apps/map.py
@@ -8,6 +8,24 @@ import pandas as pd
 
 from app import app
 
+def get_figure(year, column_name):
+    rend_df = df[df['year'] == year]
+    title = f'{column_name} in {year}'
+    fig = px.choropleth(rend_df,
+                        locations="state",
+                        color=column_name,
+                        hover_name=column_name,
+                        locationmode='USA-states',
+                        )
+    fig.update_layout(
+        paper_bgcolor='rgba(0,0,0,0)',
+        geo=dict(bgcolor='rgba(0,0,0,0)'),
+        title={'text': title, 'font': {'size': 30}},
+        geo_scope='usa',
+        margin={"r": 0, "t": 60, "l": 0, "b": 0},
+        font_color="white",
+    )
+    return fig
 
 df = pd.read_csv('assets/honeyproduction.csv')
 years = df['year'].unique()
@@ -28,7 +46,7 @@ filter_div = html.Div(
     )],
     id='filter-div')
 
-map_graph = dcc.Graph(id='map')
+map_graph = dcc.Graph(id='map', figure=get_figure(1998, 'totalprod'))
 
 layout = html.Div(
     [filter_div,
@@ -41,16 +59,4 @@ layout = html.Div(
     [Input('filter-years', 'value'),
      Input('filter-variable', 'value')])
 def update_figure(year, column_name):
-    rend_df = df[df['year'] == year]
-    title = f'{column_name} in {year}'
-    fig = px.choropleth(rend_df,
-                        locations="state",
-                        color=column_name,
-                        hover_name=column_name,
-                        locationmode='USA-states')
-    fig.update_layout(
-        title={'text': title, 'font': {'size': 30}},
-        geo_scope='usa',
-        margin={"r": 0, "t": 60, "l": 0, "b": 0}
-    )
-    return fig
+    return get_figure(year, column_name)
