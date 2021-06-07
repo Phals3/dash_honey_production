20196cb3742065f613ddc0b49a56a396699a717b
Phals3 <phalse3@gmail.com>
2021-06-08 01:23:13 +0200

    updated navbar layout

--- app/navbar.py
+++ app/navbar.py
@@ -2,17 +2,26 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
+
 def Navbar():
-    navbar = dbc.NavbarSimple(
-    children=[
-        dbc.NavItem(dbc.NavLink("Data Table", href="/apps/data_table")),
-        dbc.NavItem(dbc.NavLink("Map", href="/apps/map")),
-        dbc.NavItem(dbc.NavLink("Plots", href="/apps/plots")),
-    ],
-    brand="Home",
-    brand_href="/",
-    dark=True,
+    navbar = dbc.Navbar(
+        [
+            dbc.Row(
+                [dbc.Col(
+                    html.Img(src='assets/bee_logo.svg', height="25px")),
+                    dbc.Col(dbc.NavbarBrand(
+                        "Honey Production in USA", className="ml-2", href='/')), ],
+            ),
+            dbc.Nav([
+                dbc.NavItem(dbc.NavLink(
+                    "Data Table", href="/apps/data_table")),
+                dbc.NavItem(dbc.NavLink("Map", href="/apps/map")),
+                dbc.NavItem(dbc.NavLink("Plots", href="/apps/plots"))]
+            ),
+        ],
+        id='navbar',
     )
     return navbar
+
 
 navbar = Navbar()
